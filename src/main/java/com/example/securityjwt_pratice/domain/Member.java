f05faae1135e368a0d6e31e3dc52d11462d00ada
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-23 20:01:00 +0900

    docs: (Member) 멤버 도메인 주석추가

--- src/main/java/com/example/securityjwt_pratice/domain/Member.java
+++ src/main/java/com/example/securityjwt_pratice/domain/Member.java
@@ -21,6 +21,7 @@ public class Member {
     private String password;
     private String roles;
 
+    // ENUM으로 안하고 ,로 해서 구분해서 ROLE을 입력 -> 그걸 파싱!!
     public List<String> getRoleList() {
         if (this.roles.length() > 0) {
             return Arrays.asList(this.roles.split(","));
