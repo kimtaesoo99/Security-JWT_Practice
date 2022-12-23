83928264cd94bb0296f9c955761800f828c991d6
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-24 00:04:20 +0900

    feat: (SecurityConfig) BCryptPasswordEncoder bean 등록

--- src/main/java/com/example/securityjwt_pratice/config/SecurityConfig.java
+++ src/main/java/com/example/securityjwt_pratice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.securityjwt_pratice.config;
 
+import com.example.securityjwt_pratice.config.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,15 +20,20 @@ public class SecurityConfig {
     private final CorsConfig corsConfig;
 
     @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 사용 x
             .and()
             .apply(new MyCustomDsl())
             .and()
             .formLogin().disable()
-            .httpBasic().disable()
+            .httpBasic().disable() // 매번 요청마다 ID,PW를 들고오기 때문에 보안 취약
             .authorizeRequests(authroize -> authroize.antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
                 .antMatchers("/api/v1/manager/**")
@@ -36,12 +43,14 @@ public class SecurityConfig {
                 .anyRequest().permitAll())
             .build();
     }
+
     public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
         @Override
         public void configure(HttpSecurity http) throws Exception {
             AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
             http
-                .addFilter(corsConfig.corsFilter());
+                .addFilter(corsConfig.corsFilter())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager));
         }
     }
 
