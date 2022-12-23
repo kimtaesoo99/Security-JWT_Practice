78dc4567b787b803aece77facbf5e8358017e687
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-23 22:11:26 +0900

    refactor: (Member) 임포트 수정

--- src/main/java/com/example/securityjwt_pratice/domain/Member.java
+++ src/main/java/com/example/securityjwt_pratice/domain/Member.java
@@ -1,11 +1,11 @@
 package com.example.securityjwt_pratice.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
