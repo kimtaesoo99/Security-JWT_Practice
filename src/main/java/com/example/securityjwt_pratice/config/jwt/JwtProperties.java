1faf097b9afb6ac784dbaa690e03efe8159e3b6f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-24 01:13:41 +0900

    refactor: (JwtProperties) 비밀값 수정

--- src/main/java/com/example/securityjwt_pratice/config/jwt/JwtProperties.java
+++ src/main/java/com/example/securityjwt_pratice/config/jwt/JwtProperties.java
@@ -1,7 +1,7 @@
 package com.example.securityjwt_pratice.config.jwt;
 
 public interface JwtProperties {
-    String SECRET = "김태수"; // 우리 서버만 알고 있는 비밀값
+    String SECRET = "Secret"; // 우리 서버만 알고 있는 비밀값
     int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
