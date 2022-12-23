865cdfb62e75f98cee2771da82f6c73d1d73fc50
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-23 19:55:29 +0900

    Initial commit

---
+++ src/main/java/com/example/securityjwt_pratice/SecurityJwtPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityjwt_pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJwtPraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securityjwt_pratice/SecurityJwtPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityjwt_pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
