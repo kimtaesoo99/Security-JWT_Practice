078c02fae4fbb108e98c01672437b0caa05a9af9
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-24 00:03:11 +0900

    feat: (PrincipalDetails) PrincipalDetails Data어노테이샨 추가

--- src/main/java/com/example/securityjwt_pratice/config/auth/PrincipalDetails.java
+++ src/main/java/com/example/securityjwt_pratice/config/auth/PrincipalDetails.java
@@ -1,12 +1,14 @@
 package com.example.securityjwt_pratice.config.auth;
 
 import com.example.securityjwt_pratice.domain.Member;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Data
 public class PrincipalDetails implements UserDetails {
 
     private Member member;
